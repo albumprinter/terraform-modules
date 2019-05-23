56dd3f44466b42c8d2151ec1e82c9b47f7c668b3
Ruslan Hamzatov <ruslan.hamzatov@albelli.com>
2019-05-23 13:59:39 +0200

    removed streaming params

--- src/modules/resources/dynamo/hash_and_range/main.tf
+++ src/modules/resources/dynamo/hash_and_range/main.tf
@@ -1,10 +1,8 @@
 resource "aws_dynamodb_table" "app" {
-  name             = "${var.name}"
-  stream_enabled   = "${var.stream_enabled}"
-  stream_view_type = "${var.stream_view_type}"
-  billing_mode     = "PAY_PER_REQUEST"         # it is a better option in 99%, except high-load (24/7)
-  hash_key         = "${var.hash_key}"
-  range_key        = "${var.range_key}"
+  name         = "${var.name}"
+  billing_mode = "PAY_PER_REQUEST"  # it is a better option in 99%, except high-load (24/7)
+  hash_key     = "${var.hash_key}"
+  range_key    = "${var.range_key}"
 
   attribute = [
     {
--- src/modules/resources/dynamo/hash_and_range/variables.tf
+++ src/modules/resources/dynamo/hash_and_range/variables.tf
@@ -1,13 +1,5 @@
 variable "name" {}
 
-variable "stream_enabled" {
-  default = false
-}
-
-variable "stream_view_type" {
-  default = "NEW_AND_OLD_IMAGES"
-}
-
 variable "hash_key" {}
 
 variable "range_key" {}
