874fd3af8eb3ca5a3fcd4cf299e7a6e0d0e11835
Ruslan Hamzatov <ruslan.hamzatov@albelli.com>
2019-05-24 07:57:55 +0200

    revert test

--- src/test.tf
+++ src/test.tf
@@ -10,25 +10,32 @@ module "label" {
   source      = "./modules/resources/label"
   application = "backoffice"
   name        = "spa"
-  environment = "Test"
-  attributes  = ["Experimental"]
-  domain      = "ci/cd"
+  environment = "test"
+  attributes  = ["experimental"]
+  domain     = "ci-cd platform"
   cost_center = "3600"
   team        = "customer care technology"
 }
 
-# module "test" {
-#   source    = "./modules/resources/dynamo/hash_and_range"
-#   name      = "${module.label.id}"
-#   hash_key  = "Column1"
-#   range_key = "Column2"
-#   tags      = "${module.label.tags}"
+module "test" {
+  source        = "./modules/patterns/sns_to_sqs_with_dlq"
+  name          = "${module.label.id}"
+  delay_seconds = 60
+  tags          = "${module.label.tags}"
 
-#   providers = {
-#     aws = "aws"
-#   }
-# }
+  providers = {
+    aws = "aws"
+  }
+}
 
 output out1 {
-  value = "${module.test.arn}"
+  value = "${module.test.sns_arn}"
+}
+
+output out2 {
+  value = "${module.test.sqs_arn}"
+}
+
+output out3 {
+  value = "${module.test.dlq_arn}"
 }
