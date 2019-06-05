936039c4aa776ae021a90cccc4bb063d325c3ca7
Ruslan Hamzatov <ruslan.hamzatov@albelli.com>
2019-06-05 13:51:01 +0200

    new dynamo policy

--- src/modules/resources/iam/elasticsearch/main.tf
+++ src/modules/resources/iam/elasticsearch/main.tf
@@ -12,12 +12,12 @@ data "aws_iam_policy_document" "app" {
       "es:GetUpgradeHistory",
     ]
 
-    resources = ["${var.elasticsearch_domain_arn}"]
+    resources = ["${var.elasticsearch_domain_arn}/*"]
   }
 }
 
 resource "aws_iam_policy" "app" {
   name        = "${var.policy_name}"
-  description = "Allows read and write access to specific Elasticsearch domain"
+  description = "Grant read and write access to specific Elasticsearch domain"
   policy      = "${data.aws_iam_policy_document.app.json}"
 }
