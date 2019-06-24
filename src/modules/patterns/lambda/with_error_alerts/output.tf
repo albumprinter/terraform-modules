a3ee60c7c449a80c56295a99c041c169a1a3e258
s. galiamov <salavat.galiamov@albelli.com>
2019-06-24 11:21:34 +0200

    added more outputs

--- src/modules/patterns/lambda/with_error_alerts/output.tf
+++ src/modules/patterns/lambda/with_error_alerts/output.tf
@@ -5,3 +5,19 @@ output "app_name" {
 output "lambda_arn" {
   value = "${module.lambda.arn}"
 }
+
+output "lambda_invoke_arn" {
+  value = "${module.lambda.invoke_arn}"
+}
+
+output "role_arn" {
+  value = "${module.lambda.role_arn}"
+}
+
+output "role_name" {
+  value = "${module.lambda.role_name}"
+}
+
+output "log_group_name" {
+  value = "${module.lambda.log_group_name}"
+}
