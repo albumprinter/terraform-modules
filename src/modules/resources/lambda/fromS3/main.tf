67492de8afbfc923e495cdc210adb20e0f94207a
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-03 18:10:39 +0100

    remove non existent attribute

--- src/modules/resources/lambda/fromS3/main.tf
+++ src/modules/resources/lambda/fromS3/main.tf
@@ -1,5 +1,4 @@
 resource "aws_lambda_function" "app" {
-  fileSpecified = "${var.fileSpecified}"
   function_name = "${var.name}"
   description   = "${var.description}"
   role          = "${var.role_arn}"
