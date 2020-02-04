e475043b389989e461a67c4e88dbc0684aaf9714
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-04 10:59:45 +0100

    fix emails count

--- src/modules/resources/sns/email_subscription/main.tf
+++ src/modules/resources/sns/email_subscription/main.tf
@@ -1,11 +1,9 @@
 resource "null_resource" "sns_subscribe" {
-  emailsCount = "${length(var.emails)}"
-
   triggers = {
     sns_topic_arn = "${var.sns_topic_arn}"
   }
 
   provisioner "local-exec" {
-    command = "aws sns subscribe --topic-arn ${var.sns_topic_arn} --protocol email --notification-endpoint ${element(var.emails, emailsCount.index)} --region ${var.region}"
+    command = "aws sns subscribe --topic-arn ${var.sns_topic_arn} --protocol email --notification-endpoint ${element(var.emails, length(var.emails))} --region ${var.region}"
   }
 }
